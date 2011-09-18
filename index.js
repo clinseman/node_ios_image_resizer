460c3a14f1c9142e9b0880e71cc8751f31d9e4c8
Chris Linseman <clinseman@gmail.com>
2011-09-18 11:16:31 +0100

    added in resize and copy

--- index.js
+++ index.js
@@ -1,42 +1,46 @@
-var findit = require('findit'),
-	utils = require('./lib/utils'),
-	fs = require('fs'),
-	 _ = require('underscore')._,
-	path = require('path'),
-	argv = require('optimist').argv;
+var fs = require('fs');
+var utils = require('./lib/utils');
+var _ = require('underscore')._;
+var argv = require('optimist')
+	.usage('Resize and copy images.\nUsage: $0 -s [source] -d [destination]')
+	.demand(['s', 'd'])
+	.alias('s', 'source')
+	.describe('s', 'Source directory')
+	.alias('d', 'destination')
+	.describe('d', 'Destination directory')
+	.argv;
+var gm = require('gm');
 
-//console.log(argv.s)
-
-if(argv.s === undefined){
-	argv.s = '.';
-}
-
-var source = argv.s;
+var source = argv.s || '.';
+var dest = '/Users/clinseman/Public/resized/';
 
 console.log('attempting to read dir: ' + source);
 
 utils.findImageFiles(source, function(err, file){
 	if(err) throw err;
-	console.log(file);
-})
-
-/*
-findit.find(source, function(file, stat){
-	//console.log(file);
-	if(path.extname(file).match(/\.png$/)){
-		console.log(file);
-	}
-})
-*/
-
-/*
-fs.readdir(source, function(err, files){
-	if(err) throw err;
-	_.each(files, function(file){
-		if(path.extname(file).match(/\.png$/)){
-			console.log("file: " + file);		
-		}
+	
+	//console.log(source + file);
+	/*
+	gm(source + file).size(function(err, size){
+		if(err) throw err;
+		console.log(size);
 	})
-});
-*/
+	*/
+	
+	fs.readFile(source + file, function(err, data){
+		if(err) throw err;
+		fs.writeFile(dest + '_' + file, data, function(err){
+			if(err) throw err;
+			console.log('It\'s saved!');
+		})
+	});
+	
+	gm(source + file)
+		.resize(50, 50, '%')
+		.write(dest + file, function(err){
+			if(err) throw err;
+			console.log(this.outname);	
+		});
+	
+})
 
